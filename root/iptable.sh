209dce6b93f47732a5667ba539d83358b0e5a66e
Jongmin Kim <jmkim@hoodyprogrammer.com>
2016-08-22 20:47:43 +0900

    Initial commit

---
+++ root/iptable.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+
+#
+# Flush all current rules
+#
+iptables -F
+
+
+#
+# Access for SSH
+#
+iptables -A INPUT -p tcp --dport ssh -j ACCEPT
+
+
+#
+# Default policies
+#
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+
+#
+# Access for localhost
+#
+iptables -A INPUT -i lo -j ACCEPT
+
+
+#
+# Established and related connections
+#
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+
+#
+# Packet forwarding (for DHCP server)
+#
+iptables -A FORWARD -i eth0 -o eth0 -j ACCEPT
+
+
+#
+# IP masquarading (for DHCP server)
+#
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+
+
+#
+# Port forwarding
+#
+
+
+#
+# Access for specific port (Out->In)
+#
+
+# HTTP
+#iptables -A INPUT -p tcp --dport http -j ACCEPT
+#iptables -A INPUT -p tcp --dport 9999 -j ACCEPT
+
+# FTP
+#iptables -A INPUT -p tcp --dport ftp-data -j ACCEPT
+#iptables -A INPUT -p tcp --sport 1024: --dport 1024: -j ACCEPT
+
+# NTP
+iptables -A INPUT -p udp --dport ntp -j ACCEPT
+
+
+#
+# Save settings
+#
+iptables-save > /etc/iptables/rules.v4
+iptables-restore < /etc/iptables/rules.v4
+iptables-apply
+
+
+#
+# List rules
+#
+iptables -L -v
