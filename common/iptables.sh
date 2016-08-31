5c8024a17925e06914dbc06463edd2113d255891
Jongmin Kim <jmkim@hoodyprogrammer.com>
2016-08-31 15:50:21 +0900

    Change the IP address in iptables

--- common/iptables.sh
+++ common/iptables.sh
@@ -3,9 +3,9 @@
 #
 # Set the IP address of this machine
 #
-#SERVER_IP[0]="192.168.56.1"
+#SERVER_IP[0]="192.168.83.1"
 #SERVER_IP[1]="121.174.208.27"
-#SERVER_IP[2]="192.168.83.1"
+#SERVER_IP[2]="192.168.56.1"
 
 
 #
