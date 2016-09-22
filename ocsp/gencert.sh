26ff8c89610af5e109faa804348d7acbbda02b2b
Jongmin Kim <jmkim@hoodyprogrammer.com>
2016-09-23 01:12:39 +0900

    Apply new local area domain name

--- ocsp/gencert.sh
+++ ocsp/gencert.sh
@@ -3,7 +3,7 @@
 newcert()
 {
     local host=$1
-    openssl genrsa -out intermediate/private/${host}.key.pem 4096
+    openssl genrsa -out intermediate/private/${host}.key.pem 2048
     chmod 400 intermediate/private/${host}.key.pem
     openssl req -config intermediate/openssl.cnf -key intermediate/private/${host}.key.pem -new -sha256 -out intermediate/csr/${host}.csr.pem
     openssl ca -config intermediate/openssl.cnf -extensions server_cert -days 3750 -notext -md sha256 -in intermediate/csr/${host}.csr.pem -out intermediate/certs/${host}.cert.pem
